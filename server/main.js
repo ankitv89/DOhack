837599fa693f133081559717728d20647663f981
parag00991 <parag@tailorkraft.com>
2016-06-19 00:00:12 +0530

    added simple search functionality

--- server/main.js
+++ server/main.js
@@ -1,7 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
+
+
+Meteor.startup(function() {
+  
+    Items.insert({name: 'Blueberries'});
+    Items.insert({name: 'Strawberries'});
+    Items.insert({name: 'Steak'});
+    Items.insert({name: 'Eggs'});
+    Items.insert({name: 'Egg eggs hello bangalore'});
+  
 });
-
-
